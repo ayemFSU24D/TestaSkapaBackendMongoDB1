8e2041fd14232b56b5d6e2b218351080156744a6
Aylin <ayemfsu24@gmail.com>
2026-01-04 15:42:57 +0100

    add login code in Signin.tsx

--- client/src/pages/Signup.tsx
+++ client/src/pages/Signup.tsx
@@ -1,3 +1,42 @@
-export function Signup() {
-  return <div>Signup Page</div>;
-}
+
+import { useEffect, useState } from "react";
+import { onAuthStateChanged, User } from "firebase/auth";
+import { auth } from "../firebase";
+import GoogleLoginButton from "../components/GoogleLoginButton";
+import EmailLogin from "../components/EmailLogin";
+import { logout } from "../auth/logout";
+
+const Signup: React.FC = () => {
+  const [user, setUser] = useState<User | null>(null);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      console.log(auth.currentUser);
+    });
+
+    return unsubscribe;
+  }, []);
+
+  return (
+    <div>
+      {user ? (
+        <>
+          <h2>Inloggad som {user.email}</h2>
+          <button onClick={logout}>Logga ut</button>
+        </>
+      ) : (
+        <>
+          <GoogleLoginButton />
+          <EmailLogin />
+        </>
+      )}
+
+    </div>
+  );
+};
+
+export default Signup;
+
+
+
