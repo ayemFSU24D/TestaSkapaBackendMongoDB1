aaaf5ca369cfd078af6a464495221735e96d2d77
Aylin <ayemfsu24@gmail.com>
2026-01-04 15:41:14 +0100

    add firebase.ts

--- client/src/firebase.ts
+++ client/src/firebase.ts
@@ -1,13 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+import { getAuth } from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: VITE_FIREBASE_API_KEY, //inte nödvändigt att ha den i 
+  apiKey: "AIzaSyA9uif7kfWysBZORfJySzwwFuBO4Z_NNfc",
   authDomain: "my-exjob.firebaseapp.com",
   projectId: "my-exjob",
   storageBucket: "my-exjob.firebasestorage.app",
@@ -16,6 +16,7 @@ const firebaseConfig = {
   measurementId: "G-D4S5SRNND1"
 };
 
+
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const auth = getAuth(app);
